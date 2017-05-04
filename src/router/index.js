637a7215a1f6b73d22c4866b6bfcadfd3d1fbf89
wjt <jtwang.me@gmail.com>
2017-05-04 19:00:15 +0800

    change root router

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,12 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'Hello',
       component: Hello
     },
     {
-      path: '/mindmap',
+      path: '/',
       name: 'MainView',
       component: MainView
     }
