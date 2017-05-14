f6465a1d17edd3a76ae13030e0ba22feba69e21b
wjt <jtwang.me@gmail.com>
2017-05-14 21:33:27 +0800

    更新 Sidebar，导图、作业、课件应有对应的 Sidebar 选项。本次 commit 尝试加入模态对话框，但未能正常显示。

--- src/components/Material.vue
+++ src/components/Material.vue
@@ -2,7 +2,7 @@
 <div>
   <input v-model="file_server_addr" placeholder="file server addr">
   <p>file server is: {{ file_server_addr }}</p>
-  <div>
+    <div>
     <el-upload
         class="upload-demo"
         drag
@@ -12,7 +12,31 @@
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
       <div class="el-upload__tip" slot="tip">上传文件大小不超过 10 MB</div>
     </el-upload>
-  </div>
+    </div>
+
+
+  <!--TODO: Below is the problematic part-->
+  <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
+
+  <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+    <el-form :model="form">
+      <el-form-item label="活动名称" :label-width="formLabelWidth">
+        <el-input v-model="form.name" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="请选择活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogFormVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+    </div>
+  </el-dialog>
+<!--TODO: Problematic part ends-->
+
   </div>
 </template>
 <script>
@@ -20,7 +44,20 @@
     name: 'homework',
     data () {
       return {
-        file_server_addr: ''
+        file_server_addr: '',
+        dialogTableVisible: false,
+        dialogFormVisible: false,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px'
       }
     }
   }
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,6 @@
 <template>
-  <el-menu default-active="2" class="sidebar">
+  <div>
+  <el-menu default-active="2" class="sidebar" :default-openeds="openeds">
     <el-submenu index="1">
       <template slot="title"><i class="el-icon-message"></i>导航一</template>
       <el-menu-item-group>
@@ -15,15 +16,32 @@
         <el-menu-item index="1-4-1">选项1</el-menu-item>
       </el-submenu>
     </el-submenu>
-    <el-menu-item index="2"><i class="el-icon-menu"></i>导航二</el-menu-item>
-    <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
+    <el-submenu index="2" >
+      <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+      <el-menu-item-group>
+        <template slot="title">分组一</template>
+        <el-menu-item index="1-1" @click="">新增题目</el-menu-item>
+        <el-menu-item index="1-2">删除题目</el-menu-item>
+      </el-menu-item-group>
+      </el-submenu>
+
+    <el-submenu index="3">
+      <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+    </el-submenu>
   </el-menu>
+
+
+  </div>
 </template>
 
 <script>
   export default {
     name: 'sidebar',
-
+    data () {
+      return {
+        openeds: ['2']
+      }
+    },
     methods: {
       async save_mindmap () {
         console.log(this.$cosmos.jm.get_data())
